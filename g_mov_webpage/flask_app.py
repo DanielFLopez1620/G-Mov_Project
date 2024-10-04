3f6acee2c2b2fd5ad5b3865bef3215fef7d5db3b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-10-04 17:36:03 -0500

    Web: NGrok functioned but exceeded bandwidth

--- g_mov_webpage/flask_app.py
+++ g_mov_webpage/flask_app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, Response, redirect, url_for
+import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# stream_url = "http://10.147.19.165:8080/?action=stream" # Localhost
+# stream_url = "https://1566-191-156-233-27.ngrok-free.app/?action=stream" # Ngrok
 
 @app.route('/')
 def index():
@@ -8,8 +13,26 @@ def index():
 
 @app.route('/home')
 def home():
-    stream_url = "http://<ip_raspberry>:8080/?action=stream"
-    return render_template('home.html', stream_url=stream_url)
+    return render_template('home.html')
+
+@app.route('/video_feed')
+def video_feed():
+    # Add ngrok-skip-browser-warning header to bypass the warning page
+    headers = {
+        'ngrok-skip-browser-warning': 'true'
+    }
+
+    # Send request to the ngrok stream
+    try:
+        response = requests.get(stream_url, headers=headers, stream=True)
+        
+        if response.status_code == 200:
+            # Stream the video from the ngrok stream
+            return Response(response.iter_content(chunk_size=1024), content_type='multipart/x-mixed-replace; boundary=frame')
+        else:
+            return "Failed to fetch the video stream", 500
+    except Exception as e:
+        return f"An error occurred: {e}", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
