6d4322c590d7fc69945d46d64c4c30c48152780b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-10-04 18:16:19 -0500

    Web: Using ZeroTier seems to with remote access but not PythonAnywhere

--- g_mov_webpage/flask_app.py
+++ g_mov_webpage/flask_app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, Response, redirect, url_for
-import requests
+from flask import Flask, render_template, redirect, url_for
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-# stream_url = "http://10.147.19.165:8080/?action=stream" # Localhost
-# stream_url = "https://1566-191-156-233-27.ngrok-free.app/?action=stream" # Ngrok
+CORS(app)
 
 @app.route('/')
 def index():
@@ -13,26 +10,7 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-@app.route('/video_feed')
-def video_feed():
-    # Add ngrok-skip-browser-warning header to bypass the warning page
-    headers = {
-        'ngrok-skip-browser-warning': 'true'
-    }
-
-    # Send request to the ngrok stream
-    try:
-        response = requests.get(stream_url, headers=headers, stream=True)
-        
-        if response.status_code == 200:
-            # Stream the video from the ngrok stream
-            return Response(response.iter_content(chunk_size=1024), content_type='multipart/x-mixed-replace; boundary=frame')
-        else:
-            return "Failed to fetch the video stream", 500
-    except Exception as e:
-        return f"An error occurred: {e}", 500
+    return render_template('home.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
